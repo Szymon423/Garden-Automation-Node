0849098157351089af979b8224707152ce0ea925
Szymon-PC <wachim17@gmail.com>
2023-03-05 21:01:42 +0100

    1st push

---
+++ include/hardware.h
@@ -0,0 +1,22 @@
+#include "Arduino.h"
+
+#define LED_in_1 21
+#define LED_in_2 14
+#define LED_out_1 13
+#define LED_out_2 12
+#define LED_info_1 11
+#define LED_info_2 10
+
+#define input_1 15
+#define input_2 16
+#define output_1 17
+#define output_2 18
+
+#define sensor_1_pin 6
+#define sensor_2_pin 7
+
+void defineIO();
+void setOutput(int, bool);
+
+
+
